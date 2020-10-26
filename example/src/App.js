22f717617b09208f89332300d924ce605e510504
Nico Santoro <nicosantoro.26@gmail.com>
2020-10-26 11:55:46 +0000

    Created custom react hook that provides a random quote

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import { useMyHook } from 'use-random-quote'
+import React, { Fragment } from 'react'
+import { useRandomQuote } from 'use-random-quote'
+import './index.css'
+
+import styles from './App.module.css'
 
 const App = () => {
-  const example = useMyHook()
+  const {
+    getQuote,
+    loading: loadingQuote,
+    error: quoteError,
+    quote
+  } = useRandomQuote({ interval: 0 })
+
   return (
-    <div>
-      {example}
+    <div className={styles.main}>
+      <h2 className={styles.title}>
+        <span>Random quote generator</span>
+        <button onClick={getQuote}>Request Quote</button>
+      </h2>
+      {loadingQuote && <span>Loading...</span>}
+      {!loadingQuote && (
+        <div className={styles.quoteInfo}>
+          {quoteError ? (
+            <span>{`Error: ${quoteError}`}</span>
+          ) : (
+            <Fragment>
+              <div className={styles.quoteField}>
+                <label className={styles.quoteLabel}>Author</label>
+                <span>{quote.author}</span>
+              </div>
+              <div className={styles.quoteField}>
+                <label className={styles.quoteLabel}>Quote:</label>
+                <span>{quote.text}</span>
+              </div>
+            </Fragment>
+          )}
+        </div>
+      )}
     </div>
   )
 }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+/**
+ * A custom React Hook that provides a random quote
+ * @param {Object} options - Optional parameters.
+ * @param {number} options.interval Optional interval; if provided the hook will request a random quote at every specified interval
+ * @returns {Object} Contains quote's information such as loading, error and the text value
+ */
+export const useRandomQuote = (options = {}) => {
+  const [quote, setQuote] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { interval = 0 } = options
+  let timer = null
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
+  const getQuote = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch('https://type.fit/api/quotes')
+      const json = await response.json()
+
+      if (response.ok) {
+        const randomQuote = json[Math.floor(Math.random() * json.length)]
+        setQuote(randomQuote)
+      }
+
+      if (!response.ok) {
+        setError(json)
+      }
+
+      setLoading(false)
+    } catch (e) {
+      console.warn('Error fetching random quote', e);
+
+      setError(e.toString())
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getQuote()
+
+    if (interval) {
+      timer = window.setInterval(getQuote, interval)
+    }
+
     return () => {
-      window.clearInterval(interval)
+      if (interval) {
+        window.clearInterval(timer);
+      }
     }
   }, [])
 
-  return counter
+  return {
+    loading,
+    quote,
+    error,
+    getQuote
+  }
 }
